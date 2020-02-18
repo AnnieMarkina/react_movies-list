24164a1aba087b92d5a9e6f6dfe0b15852c0a8b7
AnnieMarkina <anniemurkina@gmail.com>
2020-02-18 14:11:29 +0200

    Updates

---
+++ precache-manifest.953b78414c738a932d157a1cab997e62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b327edc74d015be5fb6452de9fabf9f",
+    "url": "/react_movies-list/index.html"
+  },
+  {
+    "revision": "5a99663c094603f25f33",
+    "url": "/react_movies-list/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "d4476716fa7271218c2c",
+    "url": "/react_movies-list/static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "5a99663c094603f25f33",
+    "url": "/react_movies-list/static/js/2.237f35df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_movies-list/static/js/2.237f35df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4476716fa7271218c2c",
+    "url": "/react_movies-list/static/js/main.a59f9bc8.chunk.js"
+  },
+  {
+    "revision": "77d635c73f3345663d7e",
+    "url": "/react_movies-list/static/js/runtime-main.35d669bf.js"
+  }
+]);
